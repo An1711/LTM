e0ddf0bb75091e8b6d8487bd1fdc275aa61f8403
trankhaho7 <trankhahao7@gmail.com>
2025-11-15 15:59:42 +0700

    Chinh BO

--- src/main/java/model/BEAN/Account.java
+++ src/main/java/model/BEAN/Account.java
@@ -1,7 +1,7 @@
 package model.BEAN;
 
 public class Account {
-	String username,pasword;
+	String username,password;
 
 	public String getUsername() {
 		return username;
@@ -12,10 +12,10 @@ public class Account {
 	}
 
 	public String getPasword() {
-		return pasword;
+		return password;
 	}
 
 	public void setPasword(String pasword) {
-		this.pasword = pasword;
+		this.password = pasword;
 	}
 }
